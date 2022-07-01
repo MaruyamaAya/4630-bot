30e9541446c8b892a5db8475f12b35bd4ce7f199
YukkuriC <799433638@qq.com>
2022-07-01 14:00:09 +0800

    复读姬 v1.1：支持多路复读

--- src/plugins/repeater.py
+++ src/plugins/repeater.py
@@ -3,13 +3,16 @@ from collections import defaultdict
 from nonebot import on_message
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Message, Event, Bot, MessageSegment
+import time
 
 
 class CACHE:
-    REPEAT_AFTER = 5
+    REPEAT_AFTER = 4  # 在于第(REPEAT_AFTER+1)位复读
+    NUM_MEMO = 3  # 最多复读连续NUM_MEMO条消息组合 a.k.a. 间隔(NUM_MEMO-1)条消息无法打断复读
 
-    last_msg = defaultdict(Message)
-    msg_count = defaultdict(int)
+    last_msg = defaultdict(lambda: [Message() for _ in range(CACHE.NUM_MEMO)])
+    msg_count = defaultdict(lambda: [0] * CACHE.NUM_MEMO)
+    msg_time = defaultdict(lambda: [0] * CACHE.NUM_MEMO)
 
 
 def checkMsgEqual(msg1, msg2):
@@ -37,12 +40,22 @@ async def _(bot: Bot, event: Event, state: T_State):
     if not group_id:
         return
 
-    msg1 = CACHE.last_msg[group_id]
-    msg2 = event.get_message()
-    if checkMsgEqual(msg1, msg2):
-        CACHE.msg_count[group_id] += 1
-        if CACHE.msg_count[group_id] == CACHE.REPEAT_AFTER:
-            await repeater.send(msg2)
-    else:
-        CACHE.last_msg[group_id] = msg2
-        CACHE.msg_count[group_id] = 1
+    msg = event.get_message()
+    msg_memo = CACHE.last_msg[group_id]
+    cnt_memo = CACHE.msg_count[group_id]
+    time_memo = CACHE.msg_time[group_id]
+
+    # 尝试复读
+    for i in range(CACHE.NUM_MEMO):
+        last_msg = msg_memo[i]
+        if checkMsgEqual(msg, last_msg):
+            cnt_memo[i] += 1
+            if cnt_memo[i] == CACHE.REPEAT_AFTER:
+                await repeater.send(msg)
+            return
+
+    # 尝试替换
+    i_lru = min(range(CACHE.NUM_MEMO), key=lambda i: time_memo[i])
+    msg_memo[i_lru] = msg
+    cnt_memo[i_lru] = 1
+    time_memo[i_lru] = time.time()
